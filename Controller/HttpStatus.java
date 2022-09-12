2f5ee512de0a7b55153bb4e8c50881833c3ac90b
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-09-11 21:18:56 -0500

    shopping

---
+++ Controller/HttpStatus.java
@@ -0,0 +1,5 @@
+package Controller;
+
+public class HttpStatus {
+    public static final Object CREATED = "";
+}
--- Controller/ShoppingCartController.java
+++ Controller/ShoppingCartController.java
@@ -1,5 +1,6 @@
 package Controller;
-
+import java.net.URI;
+import java.net.http.HttpHeaders;
 import Saving.CartSaving;
 
 @RestController
@@ -8,8 +9,28 @@ public class ShoppingCartController {
     CartSaving cartSaving;
 
 @RequestMapping(value = "/users{idUser/cart", method = RequestMethod.POST)
-    public  @ResponseBody ResponseEntity<Void> create@ResponseBody cart cart, HttpServletRequest request) throws Exception{
-}
+    public  @ResponseBody ResponseEntity<Void> create@ResponseBody cart cart, HttpServletRequest) throws URISyntaxException{
+
+        Long id = cartSaving.save(cart);
+        HttpHeaders header = new HttpHeaders();
+        header.setLocation(new URI(request.getRequestURL() + "/" + id.toString()));
+        return new ResponseEntity<Void>(header, HttpStatus.CREATED);
+            }
+    @RequestMapping(value = "/users/{idUser}/carts/{idCart}", method = RequestMethod.PUT)
+    public @ResponseBody ResponseEntity<Void> addProduct(@PathVariable("idCart") Long idCart, @RequestParam("idProduct") Long idProduct,
+                                                         @RequestParam("quantity") Integer quantity) {
+        cartSaving.add(idCart, idProduct, quantity);
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/orders", method = RequestMethod.POST)
+    public @ResponseBody ResponseEntity<Void> ordered(@PathVariable("idCart") Long idCart, HttpServletRequest request) throws URISyntaxException {
+        Long id = cartSaving.ordered(idCart);
+        HttpHeaders header = new HttpHeaders();
+        header.setLocation(new URI(request.getRequestURL() + "/" + id.toString()));
+        return new ResponseEntity<Void>(header, HttpStatus.CREATED);
+    }
+
 }
 
 
