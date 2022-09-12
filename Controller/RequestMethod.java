6325db46263669ded4aaaf456faf8e4acfdc8d9b
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-09-11 21:05:27 -0500

    shopping

---
+++ Controller/RequestMethod.java
@@ -0,0 +1,5 @@
+package Controller;
+
+public interface RequestMethod {
+    Object POST = "";
+}
--- Controller/ShoppingCartController.java
+++ Controller/ShoppingCartController.java
@@ -1,6 +1,15 @@
 package Controller;
+
+import Saving.CartSaving;
+
 @RestController
 public class ShoppingCartController {
+@Autowired
+    CartSaving cartSaving;
 
-
+@RequestMapping(value = "/users{idUser/cart", method = RequestMethod.POST)
+    public  @ResponseBody ResponseEntity<Void> create@ResponseBody cart cart, HttpServletRequest request) throws Exception{
 }
+}
+
+
